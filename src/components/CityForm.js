8de8db1e175095acac5f444c6919e7bae133d7d5
Timothy Parker <tlparker71@gmail.com>
2022-08-17 21:55:51 -0700

    added error catch

--- src/components/CityForm.js
+++ src/components/CityForm.js
@@ -13,10 +13,11 @@ class CityForm extends Component {
         this.state = {
             query: '',
             data: {
-                display_name: '' ,
+                display_name: '',
                 latitude: '',
                 longitude: '',
                 map: '',
+                error: { status: null, message: null },
             },
         }
     }
@@ -24,20 +25,30 @@ class CityForm extends Component {
     getCity = (e) => {
         e.preventDefault();
         let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_API_KEY}&q=${this.state.query.toLowerCase()}&format=json`;
-        axios.get(url).then(response => {
-            console.log(response.data);
-            this.setState({
-                display_name: response.data[0].display_name,
-                latitude: response.data[0].lat,
-                longitude: response.data[0].lon,
-                map: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${response.data[0].lat},${response.data[0].lon}&zoom=11`
+        axios.get(url)
+            .then(response => {
+                console.log(response.data[0]);
+                this.setState({
+                    display_name: response.data[0].display_name,
+                    latitude: 'Latitude: ' + response.data[0].lat,
+                    longitude: 'Longitude: ' + response.data[0].lon,
+                    map: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${response.data[0].lat},${response.data[0].lon}&zoom=12`
+                })
+            .catch(error => {
+                this.setState({
+                    error: {
+                        status: error.response.status,
+                        message: error.response.data.message
+                    }
+                })
+                console.log(error);
             })
-        });
+            });
     }
 
     handleChange = (e) => {
         let { value } = e.target;
-        this.setState({ query: value});
+        this.setState({ query: value });
     }
 
     render() {
@@ -53,8 +64,8 @@ class CityForm extends Component {
                 <Card>
                     <Card.Img src={this.state.map} />
                     <Card.Title>{this.state.display_name}</Card.Title>
-                    <Card.Text>Latitude: {this.state.latitude}</Card.Text>
-                    <Card.Text>Longitude: {this.state.longitude}</Card.Text>
+                    <Card.Text>{this.state.latitude}</Card.Text>
+                    <Card.Text>{this.state.longitude}</Card.Text>
                 </Card>
             </Container>
 
